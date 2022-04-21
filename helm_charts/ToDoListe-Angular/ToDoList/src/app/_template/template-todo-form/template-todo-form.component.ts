f48d3c01efa53a5fe9d5cefa563c84fb62f6389a
danielbautz317 <75019856+bautzen69@users.noreply.github.com>
2022-04-21 13:58:39 +0200

    end of vid6

--- helm_charts/ToDoListe-Angular/ToDoList/src/app/_template/template-todo-form/template-todo-form.component.ts
+++ helm_charts/ToDoListe-Angular/ToDoList/src/app/_template/template-todo-form/template-todo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/_interface/todo';
 
 @Component({
   selector: 'app-template-todo-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-todo-form.component.sass']
 })
 export class TemplateTodoFormComponent implements OnInit {
+  public toDo$: ToDo;
 
-  constructor() { }
+  constructor() {
+    this.toDo$ = {
+      id: undefined,
+      position: undefined,
+      status: false,
+      label: undefined
+    }
+  }
 
   ngOnInit(): void {
   }
-
+  public createToDo(event?: any): void {
+    console.log(this.toDo$);
+    this.toDo$ = {
+      id: undefined,
+      position: undefined,
+      status: false,
+      label: undefined
+    }
+  }
 }
--- helm_charts/ToDoListe-Angular/ToDoList/src/app/_template/template-todo/template-todo.component.ts
+++ helm_charts/ToDoListe-Angular/ToDoList/src/app/_template/template-todo/template-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../_interface/todo';
 
 @Component({
   selector: 'app-template-todo',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateTodoComponent implements OnInit {
 
-  constructor() { }
+  public toDo$: ToDo;
+
+  constructor() {
+    this.toDo$ = {
+      id: 1,
+      label: 'Wie gehts',
+      status: false,
+      position: 1
+    };
+  }
 
   ngOnInit(): void {
   }
 
+  public changeCheck(event?: any): void {
+    this.toDo$.status = !this.toDo$.status;
+  }
+  public changeLabel(event?: any): void {
+    console.log(this.toDo$.label);
+  }
+  public deleteToDo(event?: any): void {
+    console.log(this.toDo$.id);
+  }
 }
