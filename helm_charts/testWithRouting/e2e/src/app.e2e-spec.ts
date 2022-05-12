08ed3560e03a5222c862b84a4d059495f7744bcf
danielbautz317 <daniel@FRL9YRPW.cw01.contiwan.com>
2022-05-12 08:44:21 +0200

    name changed

--- helm_charts/testWithRouting/e2e/src/app.e2e-spec.ts
+++ helm_charts/testWithRouting/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('testWithRouting app is running!');
+    expect(page.getTitleText()).toEqual('FlashingStationApplication app is running!');
   });
 
   afterEach(async () => {
